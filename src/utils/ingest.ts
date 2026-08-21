3fca2cbbb7cf72f4196f6ae2510ffeb1882c7428
Evan-Fallon <contact@evanfallon.com>
2026-08-21 08:21:56 -0700

    first commit

---
+++ src/utils/ingest.ts
@@ -0,0 +1,58 @@
+import glob from 'fast-glob'
+import fs from 'node:fs'
+import path from 'node:path';
+import matter from 'gray-matter'
+
+
+const vaultpath = import.meta.env.PUBLIC_PATH
+
+export interface InlineProperty {
+    line: number;
+    Path: string;
+    [key: string]: any;
+}
+
+export interface VaultNote {
+  name: string;
+  frontmatter: Record<string, any>;
+  inlineProperties: InlineProperty[];
+}
+export async function getVar(): Promise<VaultNote[]> {
+    const files = await glob('**/*.md', { cwd: vaultpath, absolute: true });
+    const mapped = files.map(file => {
+        const rawfile = fs.readFileSync(file, 'utf8');
+        let frontmatter = {}
+        try {
+            const parsed = matter(rawfile)
+            frontmatter = parsed.data
+        } catch (error) {frontmatter = "FAILED"}
+
+        const inlineProperties: InlineProperty[] = []
+        const regex = /%%\(([^:]+)::(.*?)\)%%/g;
+        for (const match of rawfile.matchAll(regex)) {
+            const matchIndex = match.index ?? 0
+            const lineNumber = rawfile.substring(0, matchIndex).split('\n').length;
+            const userInput =  match[2].trim()
+            const propEntry: InlineProperty = {
+                line: lineNumber,
+                Path: file
+            }
+            propEntry.raw = userInput
+            if (userInput.includes(" | ")) {
+                    userInput.split(" | ").forEach(pair => {
+                        const [key, value] = pair.split(": ").map(x => x.trim())
+                        if (key) propEntry[key] = value ?? ''
+                    })
+            }
+            inlineProperties.push(propEntry)
+        }
+
+        return {
+            name: path.basename(file, '.md'),
+            frontmatter: frontmatter,
+            inlineProperties: inlineProperties
+        }
+    })
+
+    return mapped
+}
