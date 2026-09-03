7ba6e47e1a9cac3b72f108321e21a764b09d07b9
Evan <contact@evanfallon.com>
2026-09-02 19:31:56 -0700

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,14 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     build: {
         format: 'file'
-    }
+    },
+    vite: {
+        build: {
+        // Set the threshold in bytes. For example, 10240 bytes = 10kB
+        // Set to 0 to completely disable inlining and force separate files
+        assetsInlineLimit: 10240, 
+        },
+    },
+    site: 'https://Evan-Fallon.github.io',
+    base: '/archive-47'
 })
